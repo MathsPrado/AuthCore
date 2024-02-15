bf61f755bc2693241a07dcdae3b5b0f28c3f498a
Matheus Prado <48390682+MathsPrado@users.noreply.github.com>
2024-02-15 08:02:55 -0300

    Update AppDbContext.cs

--- AuthCoreAPI/Context/AppDbContext.cs
+++ AuthCoreAPI/Context/AppDbContext.cs
@@ -6,7 +6,7 @@ namespace AuthCoreAPI.Context
     public class AppDbContext :  DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-        public DbSet<User> SolicitacaoProjeto { get; set; }
+        public DbSet<User> User { get; set; }
 
         protected override void OnModelCreating(ModelBuilder mb)
         {
